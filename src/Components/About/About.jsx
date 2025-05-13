cf662874f68c8ddbfa825ac5f35f5c40826febf0
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2025-05-13 10:54:08 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,52 +9,48 @@ import css_icon from "../../assets/css.svg";
 import html_icon from "../../assets/html5.svg";
 import bootstrap_icon from "../../assets/bootstrap.svg";
 import vs_icon from "../../assets/vs.svg";
-import nodeJs_icon from "../../assets/nodejs.svg"
-import postman_icon from "../../assets/postman.svg"
-import MongoDB_icon from "../../assets/MongoDB.svg"
-import mySql_icon from "../../assets/mySql.svg"
-import git_icon from "../../assets/git.svg"
-import arduino_icon from "../../assets/aurdino.svg"
+import nodeJs_icon from "../../assets/nodejs.svg";
+import postman_icon from "../../assets/postman.svg";
+import MongoDB_icon from "../../assets/MongoDB.svg";
+import mySql_icon from "../../assets/mySql.svg";
+import git_icon from "../../assets/git.svg";
+import arduino_icon from "../../assets/aurdino.svg";
 
 const About = () => {
   return (
-    <div
-      id="about"
-      className="flex flex-col items-center justify-center gap-8 m-8 sm:m-8 md:m-16"
-    >
-      <h1 className="text-5xl text-center font-semibold">About Me</h1>
-      <div className="flex flex-col md:flex-row items-center md:items-start m-8 md:m-5 gap-10">
+    <div id="about" className="flex flex-col items-center justify-center gap-8 p-8 md:p-16">
+      <h1 className="text-5xl text-center font-semibold text-gray-800">About Me</h1>
+      <div className="flex flex-col md:flex-row items-center md:items-start mt-10 gap-12">
         <div className="w-full md:w-1/2 flex justify-center">
-          <img src={profile_img} alt="Profile" className="w-3/4 rounded-lg" />
+          <img src={profile_img} alt="Profile" className="w-3/4 rounded-lg shadow-xl" />
         </div>
-        <div className="flex flex-col gap-10 w-full md:w-1/2">
-          <div className=" text-justify">
-            <p>
-              I’m Sai Ganesh Ratnala, a Final Year B.Tech student in Electronics and Computer Engineering at Sreenidhi Institute of Science and Technology (SNIST),
-              Hyderabad, graduating in 2025.
-              <br>
-              With a strong interest in Robotics and Web Development, I have built hands-on experience through diverse projects and leadership roles. 
-              I enjoy exploring new technologies, solving real-world problems, and collaborating with like-minded individuals to create impactful solutions.
+        <div className="flex flex-col gap-8 w-full md:w-1/2">
+          <p className="text-lg text-gray-700 text-justify">
+            Hello, I’m Sai Ganesh Ratnala, a passionate and dedicated Final Year B.Tech student specializing in Electronics and Computer Engineering at Sreenidhi Institute of Science and Technology (SNIST), Hyderabad. I'm set to graduate in 2025.
+            <br />
+            With a keen interest in Robotics and Web Development, I have gained hands-on experience through a variety of personal and collaborative projects. My enthusiasm for solving real-world problems through innovative technology drives me to constantly learn and improve.
+            <br />
+            I enjoy working in dynamic, fast-paced environments where I can collaborate with like-minded professionals to develop meaningful solutions. My expertise spans full-stack development, data management, and systems design.
+          </p>
 
-            </p>
-          </div>
           <div>
-            <div className=" grid lg:grid-cols-5 grid-cols-3">
-              <img src={html_icon} alt="" className=" w-40" />
-              <img src={css_icon} alt="" className=" mt-2 w-40" />
-              <img src={js_icon} alt="" className=" w-40" />
-              <img src={react_icon} alt="" className=" w-40" />
-              <img src={nodeJs_icon} alt="" className=" w-30" />
-              <img src={MongoDB_icon} alt="" className=" w-40" />
-              <img src={postman_icon} alt="" className=" w-40" />
-              <img src={tailwind_icon} alt="" className=" w-40" />
-              <img src={bootstrap_icon} alt="" className=" w-40" />
-              <img src={vs_icon} alt="" className=" w-40" />
-              <img src={mySql_icon} alt="" className=" w-40" />
-              <img src={git_icon} alt="" className=" w-40" />
-              <img src={ps_icon} alt="" className=" w-40" />
-              <img src={figma_icon} alt="" className=" w-40" />
-              <img src={arduino_icon} alt="" className=" w-40" />
+            <h2 className="text-xl font-semibold text-gray-800">Key Skills & Technologies:</h2>
+            <div className="grid lg:grid-cols-5 grid-cols-3 gap-6 mt-4">
+              <img src={html_icon} alt="HTML5" className="w-24 h-24" />
+              <img src={css_icon} alt="CSS3" className="w-24 h-24" />
+              <img src={js_icon} alt="JavaScript" className="w-24 h-24" />
+              <img src={react_icon} alt="React" className="w-24 h-24" />
+              <img src={nodeJs_icon} alt="Node.js" className="w-24 h-24" />
+              <img src={MongoDB_icon} alt="MongoDB" className="w-24 h-24" />
+              <img src={postman_icon} alt="Postman" className="w-24 h-24" />
+              <img src={tailwind_icon} alt="Tailwind CSS" className="w-24 h-24" />
+              <img src={bootstrap_icon} alt="Bootstrap" className="w-24 h-24" />
+              <img src={vs_icon} alt="Visual Studio" className="w-24 h-24" />
+              <img src={mySql_icon} alt="MySQL" className="w-24 h-24" />
+              <img src={git_icon} alt="Git" className="w-24 h-24" />
+              <img src={ps_icon} alt="Photoshop" className="w-24 h-24" />
+              <img src={figma_icon} alt="Figma" className="w-24 h-24" />
+              <img src={arduino_icon} alt="Arduino" className="w-24 h-24" />
             </div>
           </div>
         </div>
